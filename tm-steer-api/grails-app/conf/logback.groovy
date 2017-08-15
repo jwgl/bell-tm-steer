ccc7ac6455d07825fcc1b8367792d061c9ff6e70
todoubaba <todoubaba@gmail.com>
2017-08-16 01:42:19 +0800

    Upgrade to grails 3.3.0

--- tm-steer-api/grails-app/conf/logback.groovy
+++ tm-steer-api/grails-app/conf/logback.groovy
@@ -1,10 +1,24 @@
 import grails.util.BuildSettings
 import grails.util.Environment
+import org.springframework.boot.logging.logback.ColorConverter
+import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
+
+import java.nio.charset.Charset
+
+conversionRule 'clr', ColorConverter
+conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+        charset = Charset.forName('UTF-8')
+
+        pattern =
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
     }
 }
 
@@ -17,9 +31,10 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
             pattern = "%level %logger - %msg%n"
         }
     }
-//    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
+    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-else {
-    root(ERROR, ['STDOUT'])
-}
+root(ERROR, ['STDOUT'])
+
+logger("cn.edu.bnuz.bell", DEBUG)
+logger("grails.app.controllers", DEBUG)
+logger("grails.app.services", DEBUG)
